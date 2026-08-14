1bdd55b76675f80ee035f4dd2e96d84645b5a52f
Jonas <jonas@2doo.be>
2026-08-14 22:00:17 +0200

    BaseDao klasse geimplementeerd

---
+++ DataAccess.SqlServer/BaseDao.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccess.SqlServer;
+
+public abstract class BaseDao
+{
+    private readonly string _connectionString;
+
+    protected BaseDao()
+    {
+        IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddUserSecrets<BaseDao>().Build();
+        string? connectionStringBase = config.GetConnectionString("DefaultConnection");
+        SqlConnectionStringBuilder sqlBuilder = new SqlConnectionStringBuilder(connectionStringBase);
+        sqlBuilder.UserID = config["SQL:UserID"];
+        sqlBuilder.Password = config["SQL:Password"];
+        _connectionString = sqlBuilder.ConnectionString;
+    }
+
+    protected SqlConnection CreateConnection()
+    {
+        return new SqlConnection(_connectionString);
+    }
+}
