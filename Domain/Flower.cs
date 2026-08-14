83005015416dd37d560832fc8177f907b6486da1
Jonas <jonas@2doo.be>
2026-08-14 16:43:31 +0200

    Flower klasse geschreven

---
+++ Domain/Flower.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain;
+
+public class Flower(int flowerId, string name, string color, decimal price)
+{
+    public int FlowerId { get; } = flowerId;
+    public string Name { get; } = name;
+    public string Color { get; } = color;
+    public decimal Price { get; } = price;
+
+    public override string ToString()
+    {
+        return $"{FlowerId} - {Name} - {Color} - {Price:F2}";
+    }
+
+}
