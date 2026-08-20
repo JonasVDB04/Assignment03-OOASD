6d43795aa952e4cc6ba2a8e0a1c9cb6efba886f0
Jonas <jonas@2doo.be>
2026-08-20 19:48:00 +0200

    Made the user class, but still have to finish it by hashing the password

---
+++ Domain/User.cs
@@ -0,0 +1,43 @@
+﻿namespace Domain;
+
+public class User
+{
+    private const int MinimumPasswordLength = 10;
+    private int _userId;
+    private string _password;
+    public string Username { get; private set; }
+    
+    public User(string name, string password)
+    {
+        SetName(name);
+        SetPassword(password);
+    }
+
+    public void SetName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentException("Name cannot be null or empty");
+        }
+        Username = name;
+    }
+
+    public void SetPassword(string password)
+    {
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            throw new ArgumentException("Password cannot be null or whitespace");
+        }
+        
+        if (_password.Length < MinimumPasswordLength)
+        {
+            throw new ArgumentException($"Password must contain at least {MinimumPasswordLength} characters.");
+        }
+        _password = password;
+    }
+
+    public override string ToString()
+    {
+        return Username;
+    }
+}
