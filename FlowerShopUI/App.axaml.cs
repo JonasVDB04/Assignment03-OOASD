82c4dd17f2ad8ee09aea3c8f98c882a75cacabdb
Jonas <jonas@2doo.be>
2026-08-20 16:43:38 +0200

    Made my login/register screen for my gui

--- FlowerShopUI/App.axaml.cs
+++ FlowerShopUI/App.axaml.cs
@@ -2,7 +2,7 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-namespace FlowershopUI;
+namespace FlowerShopUI;
 
 public partial class App : Application
 {
--- FlowerShopUI/MainWindow.axaml.cs
+++ FlowerShopUI/MainWindow.axaml.cs
@@ -1,11 +1,22 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
-namespace FlowershopUI;
+namespace FlowerShopUI;
 
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick_Login(object? sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void Button_OnClick_Register(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
